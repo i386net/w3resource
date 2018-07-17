8331b3d904674786d2f837c9744db931dff34411
DK <i386.net@gmail.com>
2018-07-17 22:01:01 +0300

    commented strings

--- Python basic part I/14.py
+++ Python basic part I/14.py
@@ -7,18 +7,20 @@ import datetime
 #c2 = datetime.date(2018, 7, 10)
 #print(c2-c1)
 
-date1 = {}
-date2 = {}
+#date1 = {}
+#date2 = {}
 
-def enter_date(date_dict):
+def enter_date():
+    date_dict = {}
     date_dict['year'] = int(input('Year: '))
     date_dict['month'] = int(input('Month: '))
     date_dict['day'] = int(input('Day: '))
     return date_dict
 
-dd1 = enter_date(date1)
-dd2 = enter_date(date2)
+dd1 = enter_date()
+dd2 = enter_date()
 
 print(datetime.date(**dd1))
 print(datetime.date(**dd2))
-print('{} days'.format(abs(datetime.date(**dd2) - datetime.date(**dd1))))
+delta = abs(datetime.date(**dd2) - datetime.date(**dd1))
+print('{} days'.format(delta.days))
