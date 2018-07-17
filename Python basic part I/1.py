a8fbaf760f7dacd06e0ca52b27f3bddd2df0ac5b
dk_imlight <dk@i386.me>
2018-07-17 15:24:46 +0300

    some ex.

---
+++ Python basic part I/1.py
@@ -0,0 +1,9 @@
+message = """
+Twinkle, twinkle, little star, 
+\t\tHow I wonder what you are! 
+\t\t\t\tUp above the world so high, 
+\t\t\t\tLike a diamond in the sky. 
+Twinkle, twinkle, little star, 
+\t\tHow I wonder what you are"""
+
+print(message)
---
+++ Python basic part I/3.py
@@ -0,0 +1,5 @@
+import datetime
+
+t = datetime.datetime.now() #new class datetime
+print(t)
+print(t.strftime('%Y-%m-%d %H:%M:%S')) # formating string
