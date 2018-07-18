4da6bfed7f683666a6ad302b02ffc99279c6b515
DK <i386.net@gmail.com>
2018-07-18 07:15:37 +0300

    20

--- Python basic part I/20.py
+++ Python basic part I/20.py
@@ -1,1 +1,7 @@
 # 20. Write a Python program to get a string which is n (non-negative integer) copies of a given string.
+
+n = int(input('Enter number: '))
+s = input('String: ')
+
+multiply_sting = lambda s, n: s * n
+print(multiply_sting(s, n))
