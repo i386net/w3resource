3272e676882492a3dcef7b742a0e244bcc4b92c5
DK <i386.net@gmail.com>
2018-07-19 08:58:48 +0300

    29

---
+++ Python basic part I/29.py
@@ -0,0 +1,12 @@
+# 29. Write a Python program to print out a set containing
+# all the colors from color_list_1 which are not present in color_list_2.
+
+
+def get_colors(set1, set2):
+    return set([color for color in set1 if color not in set2])
+
+
+color_list_1 = set(["White", "Black", "Red"])
+color_list_2 = set(["Red", "Green"])
+uniq_colors = get_colors(color_list_1, color_list_2)
+print(uniq_colors)
