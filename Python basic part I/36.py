b29cb3f7313331bc30d07433986e562705fa8942
DK <i386.net@gmail.com>
2018-07-19 17:50:42 +0300

    36

--- Python basic part I/36.py
+++ Python basic part I/36.py
@@ -1,1 +1,5 @@
 # 36. Write a Python program to add two objects if both objects are an integer type
+
+def sumObjects(obj1, obj2):
+    if type(obj1) == int and type(obj2) == int:
+        return obj1 + obj2
