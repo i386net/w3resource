6d515f63f0e12ce1f9db7ea25c3aaf8ea4c05294
DK <i386.net@gmail.com>
2018-07-19 18:24:23 +0300

    39

---
+++ Python basic part I/39.py
@@ -0,0 +1,9 @@
+# 39
+
+present_value = 10000
+rate_of_interest_per_annum = 3.5
+years = 7
+
+future_value = present_value * ((1 + 0.01 * rate_of_interest_per_annum) ** years)
+print(round(future_value, 2))
+
