9e91de63ed67d71383b29c9f39aa372ba9f80c56
dk_imlight <dk@i386.me>
2018-07-23 13:04:58 +0300

    str -> float str -> float -> int

--- Python basic part I/48.py
+++ Python basic part I/48.py
@@ -1,4 +1,7 @@
 # 48. Write a Python program to parse a string to Float or Integer.
+s = ''
+def parse_string(s):
+    return (float(s), int(float(s)))
 
-def parse_string():
-    pass
+for n in parse_string('3434.44'):
+    print(n)
