0dcb42caeead6cfa22ad674fc26f5b5c049e9b4f
dk_imlight <dk@i386.me>
2018-07-23 12:37:44 +0300

    46 os.path.relpath

--- Python basic part I/46.py
+++ Python basic part I/46.py
@@ -1,6 +1,10 @@
 # 46. Write a python program to get the path and name of the file that is currently executing.
 
 import os
-
+# return canonical path of file
 print(os.path.realpath(__file__))
-print(__file__)
+# return a relative path to file from current dir or from an optional start dir
+try:
+    print('relpath:', os.path.relpath(__file__, start='d:'))
+except ValueError:
+    print('Wrong path!')
